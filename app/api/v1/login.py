c971f495ceeff61e0015856fc24c42f812a1e2f7
mrchakradharxyz <chakrip24@gmail..com>
2026-01-03 15:06:42 +0530

    feat(auth): Added login with username and password

--- app/api/v1/login.py
+++ app/api/v1/login.py
@@ -1,23 +1,47 @@
-from fastapi import APIRouter, HTTPException
+# from dotenv import load_dotenv
+import os
+
+import httpx
+from fastapi import APIRouter
+from pydantic import BaseModel
+
 from app.core.jwt_auth import generate_token
 
-router = APIRouter(prefix="/auth", tags=["auth"])
+auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/login")
-async def login(roll_no: str):
-    # 🔒 replace this with OTP verification later
-    if not roll_no:
-        raise HTTPException(status_code=400, detail="Invalid roll number")
+SIGN_URL = os.getenv("SIGN_URL", "")
 
-    token = generate_token({
-        "roll_no": roll_no,
-        "role": "student",
-    })
 
-    return {
-        "access_token": token,
-        "token_type": "Bearer",
-        "expires_in": 1800,
-    }
+class LoginRequest(BaseModel):
+    username: str
+    password: str
 
+
+@auth_router.post("/login")
+async def login(data: LoginRequest) -> dict:
+    async with httpx.AsyncClient(verify=False) as client:
+        response = await client.post(
+            SIGN_URL,
+            json={
+                "username": data.username,
+                "password": data.password,
+                "ipAddress": "127.0.0.1",
+                "module": "ExamCell",
+                "domain": "REMOVED_DOMAIN",
+            },
+        )
+        if response.status_code == 200:
+            token = generate_token(
+                {
+                    "roll_no": response.json()["username"],
+                    "role": response.json()["roles"],
+                }
+            )
+            return {
+                "token": token,
+                "token_type": "Bearer",
+                "expires_in": 1800,
+            }
+        else:
+            return {"error": "Login failed", "response": response.json()}
