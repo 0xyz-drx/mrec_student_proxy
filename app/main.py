5f330221902f49867c16a7115edbc6061436ead4
mrchakradharxyz <chakrip24@gmail..com>
2026-01-04 18:50:03 +0530

    fix(main): small fixes

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ app = FastAPI(title="University Proxy API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # tighten later
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 
